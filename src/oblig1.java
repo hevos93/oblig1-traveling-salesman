29eff3e4cbc1e59c6f947f6ae5ea3f36a87a25ca
Håkon <Hevos93@gmail.com>
2021-10-14 13:49:17 +0200

    Made changes to greedy method and to output

--- src/oblig1.java
+++ src/oblig1.java
@@ -6,9 +6,8 @@ class oblig1 {
         Scanner input = new Scanner(System.in);
         Random rnd = new Random();
 
-        //System.out.println("How many cities do you want to run the algorithm with?");
-        //int l = input.nextInt();
-        int l = 10;
+        System.out.println("How many cities do you want to run the algorithm with?");
+        int l = input.nextInt();
         int[][] city = new int [l][l]; //creates a new 2d array for cities
 
         for(int i=0; i<city.length; i++) { //fills the cities array with zeros
@@ -25,15 +24,15 @@ class oblig1 {
                     city[i][j] = 0;
                     city[j][i] = 0;
                 }//if
-                System.out.println("Distance from city " + (i + 1) + " to city " + (j + 1) + " is " + city[i][j]);
+                //System.out.println("Distance from city " + (i + 1) + " to city " + (j + 1) + " is " + city[i][j]);
             }//for
         }//for
-        //random(city);
+        random(city);
         greedy(city);
        }//method main
 
         public static void random(int[][] city) {
-            long startTime = System.currentTimeMillis();//creating a start timestamp
+            long startTime = System.nanoTime();//creating a start timestamp
             int[] visited = new int[city.length]; //creates a new 1d array
             for (int i = 0; i < visited.length; i++) { //Makes every value in visited zero.
                 visited[i] = 0;
@@ -54,36 +53,59 @@ class oblig1 {
                     endCity = goal;
                 }//if
             }//for
-            long endTime = System.currentTimeMillis();//creating the end timestamp
+            long endTime = System.nanoTime();//creating the end timestamp
+
+            long ns = endTime-startTime;
+            long ms = ns/100000;
+            long s = ns/1000000000;
+
             System.out.println("RANDOM METHOD: " +
                     " \n\tStart city: "+startCity+
                     " \n\tEnd city: "+endCity+
                     " \n\tTotal cost: "+(cost+city[endCity][startCity])+
-                    " \n\tTotal Time: "+(endTime-startTime)+"ms.");
+                    " \n\tTotal Time: "+ns+"ns, or "+ms+"ms, or "+s+"s.");
         }//method random
 
+
     public static void greedy(int[][] city) {
-        long startTime = System.currentTimeMillis();//creating a start timestamp
+        long startTime = System.nanoTime();//creating a start timestamp
         int[] visited = new int[city.length]; //creates a new 1d array
         for (int i = 0; i < visited.length; i++) { //Makes every value in visited zero.
             visited[i] = 0;
         }//for
 
-        Random rnd = new Random();
+        Random rnd = new Random();//Chooses a random starting city
         int start = rnd.nextInt(city.length);
+        int startCity = start;
         int goal = 0;
-        int lowest = city[start][0] + 100;
-        for (int i = 0; i< city.length; i++){
-            if (city[start][i]<lowest && i != start && city[start][i] != 0)
-                lowest = city[start][i];
-        }
-        System.out.println("From city "+(start+1)+" to city "+(goal+1)+" the cost is: "+lowest);
-        System.out.println("Check: "+city[start][goal]);
+        int lowest;
+        int cost = 0;
 
-//        System.out.println("GREEDY METHOD: " +
-//                " \n\tStart city: "+startCity+
-//                " \n\tEnd city: "+endCity+
-//                " \n\tTotal cost: "+(cost+city[endCity][startCity])+
-//                " \n\tTotal Time: "+(endTime-startTime)+"ms.");
+        for(int i = 0; i< (city.length-1); i++) {
+            lowest = city[start][0] + 100;
+            for (int j = 0; j < city.length; j++) { //calculates which city has the least cost to move to
+                if (city[start][j] < lowest && j != start && city[start][j] != 0 && visited[j] == 0) {
+                    lowest = city[start][j];
+                    goal = j;
+                }//if
+            }//for
+            visited[start] = 1;//marks the start city as visited
+            visited[goal] = 1;//marks the goal city as visited
+            start = goal; //moves to the chosen city
+            cost = cost + lowest;
+        }//for
+
+        int endCity = goal;
+        long endTime = System.nanoTime();//creating the end timestamp
+
+        long ns = endTime-startTime;
+        long ms = ns/100000;
+        long s = ns/1000000000;
+
+        System.out.println("\nGREEDY METHOD: " +
+                " \n\tStart city: "+startCity+
+                " \n\tEnd city: "+endCity+
+                " \n\tTotal cost: "+(cost+city[endCity][startCity])+
+                " \n\tTotal Time: "+ns+"ns, or "+ms+"ms, or "+s+"s.");
     }//method greedy
 }//class oblig1
