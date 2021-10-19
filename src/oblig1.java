d93865b649aeee85429a96319039c64d963fb859
Power Meat <hevos93@gmail.com>
2021-10-19 13:38:26 +0200

    Made changes to implement greedy random and updated TODO

--- src/oblig1.java
+++ src/oblig1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 
 class oblig1 {
-       public static void main(String[] args) {
+       public static void main(String[] args) { //TODO implement try catch to convert to bigint - fourth
         Scanner input = new Scanner(System.in);
         Random rnd = new Random();
 
@@ -24,14 +24,15 @@ class oblig1 {
                     city[i][j] = 0;
                     city[j][i] = 0;
                 }//if
-                //System.out.println("Distance from city " + (i + 1) + " to city " + (j + 1) + " is " + city[i][j]);
+                System.out.println("Distance from city " + (i + 1) + " to city " + (j + 1) + " is " + city[i][j]);
             }//for
         }//for
-        random(city);
-        greedy(city);
-       }//method main
+            random(city);
+            greedy(city);
+            greedyRandom(city);
+        }//method main
 
-        public static void random(int[][] city) {
+        public static void random(int[][] city) { //TODO implement an array to store the solution for random third
             long startTime = System.nanoTime();//creating a start timestamp
             int[] visited = new int[city.length]; //creates a new 1d array
             for (int i = 0; i < visited.length; i++) { //Makes every value in visited zero.
@@ -67,7 +68,7 @@ class oblig1 {
         }//method random
 
 
-    public static void greedy(int[][] city) {
+    public static void greedy(int[][] city) { //TODO implement an array to store the solution for greedy second
         long startTime = System.nanoTime();//creating a start timestamp
         int[] visited = new int[city.length]; //creates a new 1d array
         for (int i = 0; i < visited.length; i++) { //Makes every value in visited zero.
@@ -108,4 +109,46 @@ class oblig1 {
                 " \n\tTotal cost: "+(cost+city[endCity][startCity])+
                 " \n\tTotal Time: "+ns+"ns, or "+ms+"ms, or "+s+"s.");
     }//method greedy
+
+    public static void greedyRandom(int [][] city){ //TODO implement random and an array for the solution first
+        long startTime = System.nanoTime();//creating a start timestamp
+        int[] visited = new int[city.length]; //creates a new 1d array
+        for (int i = 0; i < visited.length; i++) { //Makes every value in visited zero.
+            visited[i] = 0;
+        }//for
+
+        Random rnd = new Random();//Chooses a random starting city
+        int start = rnd.nextInt(city.length);
+        int startCity = start;
+        int goal = 0;
+        int lowest;
+        int cost = 0;
+
+        for(int i = 0; i< (city.length-1); i++) {
+            lowest = city[start][0] + 100;
+            for (int j = 0; j < city.length; j++) { //calculates which city has the least cost to move to
+                if (city[start][j] < lowest && j != start && city[start][j] != 0 && visited[j] == 0) {
+                    lowest = city[start][j];
+                    goal = j;
+                }//if
+            }//for
+            visited[start] = 1;//marks the start city as visited
+            visited[goal] = 1;//marks the goal city as visited
+            start = goal; //moves to the chosen city
+            cost = cost + lowest;
+        }//for
+
+        int endCity = goal;
+        long endTime = System.nanoTime();//creating the end timestamp
+
+        long ns = endTime-startTime;
+        long ms = ns/100000;
+        long s = ns/1000000000;
+
+        System.out.println("\nGREEDY RANDOM METHOD: " +
+                " \n\tStart city: "+startCity+
+                " \n\tEnd city: "+endCity+
+                " \n\tTotal cost: "+(cost+city[endCity][startCity])+
+                " \n\tTotal Time: "+ns+"ns, or "+ms+"ms, or "+s+"s.");
+    }//method greedyRandom
 }//class oblig1
